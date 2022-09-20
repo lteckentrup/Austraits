48d59cabddd84117232748ab276c644668d76f81
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-09-20 16:41:39 +1000

    Update milk_Austraits.py

--- milk_Austraits.py
+++ milk_Austraits.py
@@ -86,7 +86,7 @@ def grab_trait(trait_name):
                           (df_lats.dataset_id == di)].loc[:,'value'].iloc[0]
             lats.append(float(lat))
         except (IndexError,ValueError):
-            lats.append('not_defined')
+            lats.append(np.nan)
 
     ### Get longitude
     for sn,di in zip(SN,DI):
@@ -95,7 +95,7 @@ def grab_trait(trait_name):
                           (df_lons.dataset_id == di)].loc[:,'value'].iloc[0]
             lons.append(float(lon))
         except (IndexError,ValueError):
-            lons.append('not_defined')
+            lons.append(np.nan)
 
     df_trait['lat'] = lats
     df_trait['lon'] = lons
@@ -125,12 +125,19 @@ def get_wooddens(veggroup):
         except IndexError:
             pass
 
-    print(veggroup)
+    print(veggroup+' median')
     print(np.median(wooddens))
-
+    print(veggroup+' 25 percentile')
+    print(np.quantile(wooddens,0.25))
+    print(veggroup+' 75 percentile')
+    print(np.quantile(wooddens,0.75))   
+    
 get_wooddens('tree')
 get_wooddens('shrub')
-
+ 
+#-------------------------------------------------------------------------------
+### Update calcsla function
+   
 def isla_params(leaftype):
     ### Define equation based on Reich et al., 1992 - calculate SLA from leaflong
     df_SLA = grab_trait('specific_leaf_area')
@@ -165,51 +172,188 @@ def isla_params(leaftype):
     print(ransac.estimator_.coef_)
     print(ransac.estimator_.intercept_)
 
+isla_params('broadleaf')
+isla_params('needle')
+
+#-------------------------------------------------------------------------------
 ### Get leaf lifespan
-def get_lifespan(vegtype,phen,leaftype,region):
-    df_lifespan = grab_trait('leaf_lifespan')
+df_lifespan = grab_trait('leaf_lifespan')
 
-    if vegtype=='tree':
-        df_lifespan_veg = df_lifespan[df_lifespan['PGF']=='tree']
-        # df_lifespan_veg_phen = df_lifespan_veg[df_lifespan_veg['PHEN'].str.contains(phen)]
-        df_lifespan_veg_phen = df_lifespan_veg[df_lifespan_veg['PHEN']==phen]
-        df_lifespan_veg = df_lifespan_veg[df_lifespan_veg['LeafType']==leaftype]
+### Grasses
+print(df_lifespan[(df_lifespan['PGF']=='herb')|(df_lifespan['PGF']=='graminoid')])
+### only six entries, either 4 or 24?? not enough, also no C4. Use SLA equation
 
-        if region=='tropical':
-            df_lifespan_veg_phen = df_lifespan_veg_phen[df_lifespan_veg_phen['lat'] >= -30]
-        else:
-            df_lifespan_veg_phen = df_lifespan_veg_phen[df_lifespan_veg_phen['lat'] < -30]
+### Tropical evergreen trees: no data
+print('TrBE')
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='evergreen')&
+                  (df_lifespan['LeafType']=='broadleaf')&
+                  (df_lifespan['lat']>-23.5)].value.dropna().astype(float).median()/12)
 
-        ### Divide by 12: LPJ takes leaf lifespan in years
-        print(vegtype)
-        print(phen)
-        print(leaftype)
-        print(region)
-        print('sample size: '+str(len(df_lifespan_veg_phen)))
-        print(df_lifespan_veg_phen.value.median()/12)
+### Tropical raingreen trees: no data
+print('TrBR')
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='drought_deciduous')&
+                  (df_lifespan['LeafType']=='broadleaf')&
+                  (df_lifespan['lat']>-23.5)].value.dropna().astype(float).median()/12)
 
-    elif vegtype=='grass':
-        df_lifespan_veg = df_lifespan[df_lifespan['PGF'].str.contains('herb|graminoid')]
 
-        ### Divide by 12: LPJ takes leaf lifespan in years
-        print(vegtype)
-        print('sample size: '+str(len(df_lifespan_veg)))
-        print(df_lifespan_veg.value.median()/12)
+### Temperate broadleaf evergreen trees, 20 values
+print('Median TeBE')
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='evergreen')&
+                  (df_lifespan['LeafType']=='broadleaf')&
+                  (df_lifespan['lat']<-23.5)].value.dropna().astype(float).median()/12)
 
-get_lifespan('tree','semi_deciduous','broadleaf','tropical')
-get_lifespan('tree','evergreen','broadleaf','tropical')
-get_lifespan('tree','deciduous','broadleaf','tropical')
-get_lifespan('tree','evergreen','broadleaf','temperate')
-get_lifespan('tree','deciduous','broadleaf','temperate')
-get_lifespan('tree','semi_deciduous','broadleaf','temperate')
-get_lifespan('tree','evergreen','needle','temperate')
-get_lifespan('grass','','','')
+print('25 percentile TeBE')
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='evergreen')&
+                  (df_lifespan['LeafType']=='broadleaf')&
+                  (df_lifespan['lat']<-23.5)].value.dropna().astype(float).quantile(0.25)/12)
 
-### Get k_latosa
+print('75 percentile TeBE')
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='evergreen')&
+                  (df_lifespan['LeafType']=='broadleaf')&
+                  (df_lifespan['lat']<-23.5)].value.dropna().astype(float).quantile(0.75)/12)
 
-df = grab_trait('huber_value')
-### Tropical broadleaf:
-1/df[(df['PGF']=='tree')&(df['lat']>-30)&(df['LeafType']=='broadleaf')].value.median()
+### Temperate broadleaf deciduous trees: no data
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='deciduous')&
+                  (df_lifespan['LeafType']=='broadleaf')&
+                  (df_lifespan['lat']<-23.5)].value.dropna().astype(float).median()/12)
 
-### Temperate broadleaf:
-1/df[(df['PGF']=='tree')&(df['lat']<-30)&(df['LeafType']=='broadleaf')].value.median()
+### Temperate needleleaf evergreen trees: no data
+print(df_lifespan[(df_lifespan['PGF']=='tree')&
+                  (df_lifespan['PHEN']=='evergreen')&
+                  (df_lifespan['LeafType']=='needle')&
+                  (df_lifespan['lat']<-30)].value.dropna().astype(float).median()/12)
+
+### Grass: only six values, all c3, some 4 months, some 24?
+print(df_lifespan[(df_lifespan['PGF']=='herb')|
+                  (df_lifespan['PGF']=='graminoid')].value.dropna().astype(float).median()/12)
+
+#-------------------------------------------------------------------------------
+### For the missing data we calculate leaf life span from specific leaf area.
+### Lifespan is also needed for ctonmin so keep lifespan instead of updating
+### sla following updated equation
+
+### Get specific leaf area
+df_SLA = grab_trait('specific_leaf_area')
+
+### Grasses
+df_grass = df_SLA[(df_SLA['PGF']=='herb')|(df_SLA['PGF']=='graminoid')]
+print('Median C3 grass')
+print(df_grass[df_grass['PHOT'] == 'c3'].value.dropna().astype(float).median())
+print('25 percentile C3 grass')
+print(df_grass[df_grass['PHOT'] == 'c3'].value.dropna().astype(float).quantile(0.25))
+print('75 percentile C3 grass')
+print(df_grass[df_grass['PHOT'] == 'c3'].value.dropna().astype(float).quantile(0.75))
+
+print('Median C4 grass')
+print(df_grass[df_grass['PHOT'] == 'c4'].value.dropna().astype(float).median())
+print('25 percentile C4 grass')
+print(df_grass[df_grass['PHOT'] == 'c4'].value.dropna().astype(float).quantile(0.25))
+print('75 percentile 4 grass')
+print(df_grass[df_grass['PHOT'] == 'c4'].value.dropna().astype(float).quantile(0.75))
+
+### Tropical evergreen trees
+print('Median TrBE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']>-23.5)].value.dropna().astype(float).median())
+print('25 percentile TrBE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']>-23.5)].value.dropna().astype(float).quantile(0.25))
+print('75 percentile TrBE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']>-23.5)].value.dropna().astype(float).quantile(0.75))
+
+### Tropical evergreen trees. Leaftype undefined; checked and they are BDL
+print('Median TrBR')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='drought_deciduous')&
+             (df_SLA['lat']>-23.5)].value.dropna().astype(float).median())
+### only six :(
+print('25 percentile TrBR')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='drought_deciduous')&
+             (df_SLA['lat']>-23.5)].value.dropna().astype(float).quantile(0.25))
+print('75 percentile TrBR')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='drought_deciduous')&
+             (df_SLA['lat']>-23.5)].value.dropna().astype(float).quantile(0.75))
+
+### Temperate evergreen trees
+print('Median TeBE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).median())
+print('25 percentile TeBE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).quantile(0.25))
+print('75 percentile TeBE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).quantile(0.75))
+
+### Temperate summergreen trees
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='deciduous')&
+             (df_SLA['LeafType']=='broadleaf')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).median())
+
+### Temperate evergreen NDL trees
+print('Median TeNE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='needle')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).median())
+print('25 percentile TeNE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='needle')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).quantile(0.25))
+print('75 percentile TeNE')
+print(df_SLA[(df_SLA['PGF']=='tree')&
+             (df_SLA['PHEN']=='evergreen')&
+             (df_SLA['LeafType']=='needle')&
+             (df_SLA['lat']<-23.5)].value.dropna().astype(float).quantile(0.75))
+
+#-------------------------------------------------------------------------------
+
+### Leaf to sapwood area ratio. Inverse of huber_value. 
+### Drop drought affected vegetation
+df_klatosa = grab_trait('huber_value')
+
+### All trees
+print('Median tree')
+print(1/df_klatosa[(df_klatosa['context_name']!='Drought')&
+                   (df_klatosa['PGF']=='tree')].value.dropna().astype(float).median())
+print('25 percentile tree')
+print(1/df_klatosa[(df_klatosa['context_name']!='Drought')&
+                   (df_klatosa['PGF']=='tree')].value.dropna().astype(float).quantile(0.25))
+print('75 percentile tree')
+print(1/df_klatosa[(df_klatosa['context_name']!='Drought')&
+                   (df_klatosa['PGF']=='tree')].value.dropna().astype(float).quantile(0.75))
+### All shrubs
+print('Median shrub')
+print(1/df_klatosa[(df_klatosa['context_name']!='Drought')&
+                   (df_klatosa['PGF']=='shrub')].value.dropna().astype(float).median())
+print('25 percentile shrub')
+print(1/df_klatosa[(df_klatosa['context_name']!='Drought')&
+                   (df_klatosa['PGF']=='shrub')].value.dropna().astype(float).quantile(0.25))
+print('75 percentile shrub')
+print(1/df_klatosa[(df_klatosa['context_name']!='Drought')&
+                   (df_klatosa['PGF']=='shrub')].value.dropna().astype(float).quantile(0.75))
+
+#-------------------------------------------------------------------------------
